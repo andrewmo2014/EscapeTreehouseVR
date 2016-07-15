ecec209b6422cb9b0144b0ecc18e1967b0d62c07
Andrew Moran <andrewmo@mit.edu>
2016-07-14 19:34:29 -0700

    Added lock and key to treehouse for MVP

---
+++ Assets/Scripts/LockController.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Animator))]
+public class LockController : MonoBehaviour {
+
+    public GameObject gate;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter( Collider col )
+    {
+        if( col.CompareTag( "key"))
+        {
+            Destroy(col.gameObject);
+            GetComponent<Animator>().SetBool("Insert", true);
+        }
+    }
+
+    public void ActivateGate()
+    {
+        if( gate.GetComponent<Animator>() != null)
+        {
+            Animator anim = gate.GetComponent<Animator>();
+            anim.SetBool("Open", true);
+        }
+    }
+}
